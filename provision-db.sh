8645e4ae9370cb09ef5e558fb4c7c0f5da717d8d
ZantaCruz <samuelizquierdo98@gmail.com>
2025-10-28 11:50:15 -0500

    Finalizacion taller

--- provision-db.sh
+++ provision-db.sh
@@ -1,1 +1,17 @@
-#debes completar este archivo con los comandos necesarios para provisionar la base de datos
+#!/usr/bin/env bash
+
+# Actualiza los paquetes del sistema
+sudo apt-get update -y
+
+# Instala PostgreSQL
+sudo apt-get install -y postgresql postgresql-contrib
+
+# Inicia el servicio
+sudo systemctl enable postgresql
+sudo systemctl start postgresql
+
+# Crea un usuario y base de datos
+sudo -u postgres psql -c "CREATE USER vagrant WITH PASSWORD 'vagrant';"
+sudo -u postgres psql -c "CREATE DATABASE taller;"
+sudo -u postgres psql -d taller -c "CREATE TABLE personas (id SERIAL PRIMARY KEY, nombre VARCHAR(50));"
+sudo -u postgres psql -d taller -c "INSERT INTO personas (nombre) VALUES ('Samuel'), ('Deyton'), ('Sebastian');"
